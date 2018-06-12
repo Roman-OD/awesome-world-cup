442506f8d9b1778b137f72e448a00be798190b97
Roman O'Dowd <roman.odowd@hotmail.de>
2018-06-13 00:35:27 +0100

    loading fixtures from data

--- imports/ui/templates/games/detail/UpcomingMatches.js
+++ imports/ui/templates/games/detail/UpcomingMatches.js
@@ -7,11 +7,21 @@ Template.UpcomingMatches.onCreated(function(){
         {homeTeam: 'Belgium', homeFlag: 'be.png', awayTeam: 'Spain', awayFlag: 'es.png',
             stadium: 'Luzhniki Stadium', date: 'Thursday 15 of June, 12:00', id: '456', stage: 'Group Stage - Matchday 1 of 3'}
     ]
-    this.matches = new ReactiveVar(matchList)
+    this.matches = new ReactiveVar([])
+
+    fetch('https://raw.githubusercontent.com/openfootball/world-cup.json/master/2018/worldcup.json')
+
+    .then(res => res.json())
+
+    .then(body =>data = body)
+
+    .then(data => this.matches.set(data.rounds))
+
+    .then(()=> console.log(this.matches.get()));
 })
 
 Template.UpcomingMatches.helpers({
-    getMatches: function () {
+    getMatchDays: function () {
         return Template.instance().matches.get()
     }
 })
