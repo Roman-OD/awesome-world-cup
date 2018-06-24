080a63acfaf8d4aa52d66902e36abd9365e8f17f
kevin-hicher <kevin.hicher@metixmedical.co.uk>
2018-06-24 18:05:51 +0100

    Fixed match times

--- imports/startup/client/global-helpers.js
+++ imports/startup/client/global-helpers.js
@@ -3,4 +3,11 @@ import { Template } from 'meteor/templating';
 
 Template.registerHelper('getUserId', function () {
     return Meteor.userId()
-})
+});
+
+Template.registerHelper('convertTime', function (time, timezone) {
+  const hours = time.split(':')[0];
+  const minutes = time.split(':')[1];
+  const offset = timezone.split('+')[1] - 1;
+  return `${hours - offset}:${minutes}`;
+});
